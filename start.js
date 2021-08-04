9e0443108a3deb9163e006403db01dd273a9716c
1poundSOCKS <mathew.coburn@gmail.com>
2021-08-04 22:15:29 +0100

    initial files

---
+++ start.js
@@ -0,0 +1,12 @@
+var express = require('express');
+const path = require('path');
+
+var app = express();
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get('/', function(req, res) {
+    res.sendFile('./index.html', { root: __dirname });
+});
+
+app.listen(8080);
