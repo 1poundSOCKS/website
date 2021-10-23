2d4b6a34c0fa463afc7b8b9af5c0b8b31cb2cbf8
1poundSOCKS <mathew.coburn@gmail.com>
2021-10-23 10:26:51 +0100

    Add topo implemented

--- public/scripts/crag_load.js
+++ public/scripts/crag_load.js
@@ -27,14 +27,48 @@ let LoadTopoList = async (cragId) => {
   return response.json();
 }
 
+let LoadTopoTable = async (cragId) => {
+  const topoList = await LoadTopoList(cragId);
+  const tableBody=document.getElementById('topo-table-body');
+  UpdateTable(tableBody, topoList);
+}
+
+let AddTopo = async (topoName, cragId) => {
+  const response = await fetch('/data/add_topo', {
+    method: 'post',
+    headers: {
+      'Accept': 'application/json, text/plain, */*',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({name: topoName, parent_id: cragId})
+  });
+
+  return response.json();
+}
+
 let LoadPage = async (cragId) => {
   const cragData = await LoadCragData(cragId);
   const cragName=document.getElementById('crag-name');
   cragName.innerHTML = `${cragData.results.parent_data.name} - ${cragData.results.name}`;
 
-  const topoList = await LoadTopoList(cragId);
-  const tableBody=document.getElementById('topo-table-body');
-  UpdateTable(tableBody, topoList);
+  LoadTopoTable(cragId);
+
+  const createButton = document.getElementById('add-topo');
+  const dialog = document.getElementById('add-topo-dialog');
+  createButton.onclick = () => {
+    dialog.showModal();
+  }
+
+  dialog.addEventListener('close', async () => {
+    const rv = dialog.returnValue;
+    if( rv == 'confirm' ) {
+      const topoName = document.getElementById('topo-name').value;
+      const response = await AddTopo(topoName, cragId);
+      // TODO: check response
+      // {"acknowledged":true,"insertedId":"61726db5a7488601199a1b12"}
+      LoadTopoTable(cragId);
+    }
+  });
 }
 
 const queryString = window.location.search;
