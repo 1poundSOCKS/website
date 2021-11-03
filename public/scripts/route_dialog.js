c1be96d46f7c15cf2bed8135b7738bf8256d2d89
1poundSOCKS <mathew.coburn@gmail.com>
2021-11-03 19:57:19 +0000

    Route table editing partially implemented

---
+++ public/scripts/route_dialog.js
@@ -0,0 +1,229 @@
+let GetRouteTypeSelection = () => {
+  return [
+    {key: 'trad', value: 'Trad'},
+    {key: 'sport', value: 'Sport'},
+    {key: 'boulder', value: 'Boulder'}
+  ]
+}
+
+let GetGradeSystemSelection = () => {
+  return [
+    {key: 'uk', value: 'UK'},
+    {key: 'french', value: 'French'},
+    {key: 'font', value: 'Font'},
+    {key: 'v', value: 'V'}
+  ]
+}
+
+let GetGradeSelection = (system) => {
+  switch( system ) {
+    case 'uk':
+      return [
+        {key: 'm', value: 'Mod'}, 
+        {key: 'd', value: 'Diff'},
+        {key: 'vd', value: 'VDiff'},
+        {key: 'hvd', value: 'HVD'},
+        {key: 's', value: 'Severe'},
+        {key: 'hs', value: 'HS'},
+        {key: 'vs', value: 'VS'},
+        {key: 'hvs', value: 'HVS'},
+        {key: 'e1', value: 'E1'},
+        {key: 'e2', value: 'E2'},
+        {key: 'e3', value: 'E3'},
+        {key: 'e4', value: 'E4'},
+        {key: 'e5', value: 'E5'},
+        {key: 'e6', value: 'E6'},
+        {key: 'e7', value: 'E7'},
+        {key: 'e8', value: 'E8'},
+        {key: 'e9', value: 'E9'},
+        {key: 'e10', value: 'E10'},
+        {key: 'e11', value: 'E11'},
+        {key: 'e12', value: 'E12'}
+      ];
+    case 'french':
+      return [
+        {key: '1', value: 'F1'}, 
+        {key: '1+', value: 'F1+'},
+        {key: '2', value: 'F2'},
+        {key: '2+', value: 'F2+'},
+        {key: '3', value: 'F3'},
+        {key: '3+', value: 'F3+'},
+        {key: '4', value: 'F4'},
+        {key: '4+', value: 'F4+'},
+        {key: '5a', value: 'F5a'},
+        {key: '5b', value: 'F5b'},
+        {key: '5c', value: 'F5c'},
+        {key: '6a', value: 'F6a'},
+        {key: '6a+', value: 'F6a+'},
+        {key: '6b', value: 'F6b'},
+        {key: '6b+', value: 'F6b+'},
+        {key: '6c', value: 'F6c'},
+        {key: '6c+', value: 'F6c+'},
+        {key: '7a', value: 'F7a'},
+        {key: '7a+', value: 'F7a'},
+        {key: '7b', value: 'F7b'},
+        {key: '7b+', value: 'F7b'},
+        {key: '7c', value: 'F7c'},
+        {key: '7c+', value: 'F7c'},
+        {key: '8a', value: 'F8a'},
+        {key: '8a+', value: 'F8a+'},
+        {key: '8b', value: 'F8b'},
+        {key: '8b+', value: 'F8b+'},
+        {key: '8c', value: 'F8c'},
+        {key: '8c+', value: 'F8c+'},
+        {key: '9a', value: 'F9a'},
+        {key: '9a+', value: 'F9a+'},
+        {key: '9b', value: 'F9b'},
+        {key: '9b+', value: 'F9b+'},
+        {key: '9c', value: 'F9c'},
+        {key: '9c+', value: 'F9c+'}
+      ];
+      default:
+        return [];
+  }
+}
+
+let GetAdditionalGradeSelection = (system) => {
+  switch( system ) {
+    case 'uk':
+      return [
+        {key: '4a', value: '4a'}, 
+        {key: '4b', value: '4b'},
+        {key: '4c', value: '4c'},
+        {key: '5a', value: '5a'},
+        {key: '5b', value: '5b'},
+        {key: '5c', value: '5c'},
+        {key: '6a', value: '6a'},
+        {key: '6b', value: '6b'},
+        {key: '6c', value: '6c'},
+        {key: '7a', value: '7a'},
+        {key: '7b', value: '7b'},
+        {key: '7c', value: '7c'}
+      ];
+    default:
+      return [];
+  }
+}
+
+let GetDisplayValueFromStoredValue = (values) => {
+  let object = {};
+  values.forEach( value => {
+    object[value.key] = value.value;
+  })
+
+  return object;
+}
+
+let GetStoredValueFromDisplayValue = (values) => {
+  let object = {};
+  values.forEach( value => {
+    object[value.value] = value.key;
+  })
+
+  return object;
+}
+
+let GetRouteTypeDisplayObject = () => GetDisplayValueFromStoredValue(GetRouteTypeSelection());
+let GetRouteTypeStorageObject = () => GetStoredValueFromDisplayValue(GetRouteTypeSelection());
+let GetGradeSelectionObject = (system) => GetDisplayValueFromStoredValue(GetGradeSelection(system));
+let GetAdditionalGradeSelectionObject = (system) => GetDisplayValueFromStoredValue(GetAdditionalGradeSelection(system));
+
+let GetRouteTypeForDisplay = (storageValue) => GetRouteTypeDisplayObject()[storageValue];
+let GetRouteTypeForStorage = (displayValue) => GetRouteTypeStorageObject()[displayValue];
+
+let GetDisplayGrade = (grade) => {
+  if( grade == undefined ) return '';
+  const gradeSelection = GetGradeSelectionObject(grade.system);
+  const additionalGradeSelection = GetAdditionalGradeSelectionObject(grade.system);
+  return (additionalGradeSelection[grade.additional_value] == undefined ) ? 
+  gradeSelection[grade.value] : `${gradeSelection[grade.value]} ${additionalGradeSelection[grade.additional_value]}`;
+}
+
+let GetRouteGradeValue = (system) => {
+  switch( system ) {
+    case 'uk':
+      return document.getElementById('route-grade').value;
+    case 'french':
+      return document.getElementById('route-grade').value;
+  }
+}
+
+let GetAdditionalRouteGradeValue = (system) => {
+  switch( system ) {
+    case 'uk':
+      return document.getElementById('additional-route-grade').value;
+    default:
+      return undefined;
+  }
+}
+
+let GetSelectionHtml = (selection) => {
+  let html = [];
+  selection.forEach( item => {
+    html.push(`<option value="${item.key}">${item.value}</option>`);
+  });
+  return html.join('');
+}
+
+let GetRouteTypeSelectionHtml = () => GetSelectionHtml(GetRouteTypeSelection());
+let GetGradeSystemSelectionHtml = () => GetSelectionHtml(GetGradeSystemSelection());
+let GetGradeSelectionHtml = (system) => GetSelectionHtml(GetGradeSelection(system));
+
+let GetFullGradeSelectionHtml = (system) => {
+  let gradeHtml = [];
+  gradeHtml.push(`<label for="route-grade">Route grade:</label>`);
+  gradeHtml.push(`<select id="route-grade"><option value="" selected="selected"></option>`);
+  gradeHtml.push(GetGradeSelectionHtml(system));
+  gradeHtml.push('</select>');
+
+  const additionalGradeDefs = GetAdditionalGradeSelection(system);
+  if( additionalGradeDefs.length > 0 ) {
+    gradeHtml.push(`<select id="additional-route-grade"><option value="" selected="selected"></option>`);
+    additionalGradeDefs.forEach( def => {
+      gradeHtml.push(`<option value="${def.key}">${def.value}</option>`);
+    });
+    gradeHtml.push('</select>');
+  }
+  return gradeHtml.join('');
+}
+
+let InitRouteDialog = (route) => {
+  const routeType = document.getElementById('route-type');
+  routeType.innerHTML = GetRouteTypeSelectionHtml();
+
+  const gradeSystem = document.getElementById('route-grade-system');
+  gradeSystem.innerHTML = GetGradeSystemSelectionHtml();
+
+  gradeSystem.onchange = () => document.getElementById('system-specific-grade').innerHTML = GetFullGradeSelectionHtml(gradeSystem.value);
+
+  document.getElementById('route-name').value = (route.name == undefined) ? '' : route.name;
+  document.getElementById('route-type').value = (route.type == undefined) ? '' : route.type;
+  document.getElementById('route-grade-system').value = (route.grade == undefined || route.grade.system == undefined) ? '' : route.grade.system;
+
+  document.getElementById('system-specific-grade').innerHTML = GetFullGradeSelectionHtml(gradeSystem.value);
+
+  document.getElementById('route-grade').value = (route.grade == undefined || route.grade.value == undefined) ? '' : route.grade.value;
+}
+
+let GetRouteFromDialog = () => {
+  const routeName = document.getElementById('route-name').value;
+  const routeType = document.getElementById('route-type').value;
+  const routeGradeSystem = document.getElementById('route-grade-system').value;
+  const routeGradeValue = GetRouteGradeValue(routeGradeSystem);
+  const additionalRouteGradeValue = GetAdditionalRouteGradeValue(routeGradeSystem);
+  const routeGrade = {system: routeGradeSystem, value: routeGradeValue, additional_value: additionalRouteGradeValue};
+  const route = {id: -1, name: routeName, type: routeType, grade: routeGrade };
+  return route;
+}
+
+let EditRouteDialog = (route, onconfirm) => {
+  InitRouteDialog(route);
+  const dialog = document.getElementById('route-dialog');
+  dialog.onclose = (event) => {
+    const rv = dialog.returnValue;
+    if( rv == 'confirm' ) {
+      onconfirm(GetRouteFromDialog());
+    }
+  }
+  dialog.showModal();
+}
--- public/scripts/topo_load.js
+++ public/scripts/topo_load.js
@@ -34,150 +34,55 @@ function Resize() {
   drawTopoRoutes(base_image, canvas);
 }
 
-let GetGradeSelection = (system) => {
-  switch( system ) {
-    case 'uk':
-      return [
-        {key: 'm', value: 'Mod'}, 
-        {key: 'd', value: 'Diff'},
-        {key: 'vd', value: 'VDiff'},
-        {key: 'hvd', value: 'HVD'},
-        {key: 's', value: 'Severe'},
-        {key: 'hs', value: 'HS'},
-        {key: 'vs', value: 'VS'},
-        {key: 'hvs', value: 'HVS'},
-        {key: 'e1', value: 'E1'}
-      ];
-    case 'french':
-      return [
-        {key: '1', value: 'F1'}, 
-        {key: '1+', value: 'F1+'},
-        {key: '2', value: 'F2'},
-        {key: '2+', value: 'F2+'},
-        {key: '3', value: 'F3'},
-        {key: '3+', value: 'F3+'},
-        {key: '4', value: 'F4'},
-        {key: '4+', value: 'F4+'},
-        {key: '5a', value: 'F5a'}
-      ];
-      default:
-        return [];
-  }
+let CreateRowFromObject = (route) => {
+  let row = document.createElement('tr');
+  row.innerHTML = 
+  `<td><input id="route-checkbox" class="route-checkbox" type="checkbox"/></td>
+  <td>${route.id}</td>
+  <td>${route.name}</td>
+  <td>${GetRouteTypeForDisplay(route.type)}</td>
+  <td>${GetDisplayGrade(route.grade)}</td>`;
+  return row;
 }
 
-let GetAdditionalGradeSelection = (system) => {
-  switch( system ) {
-    case 'uk':
-      return [
-        {key: '4a', value: '4a'}, 
-        {key: '4b', value: '4b'},
-        {key: '4c', value: '4c'},
-        {key: '5a', value: '5a'},
-        {key: '5b', value: '5b'},
-        {key: '5c', value: '5c'},
-        {key: '6a', value: '6a'},
-        {key: '6b', value: '6b'},
-        {key: '6c', value: '6c'},
-        {key: '7a', value: '7a'},
-        {key: '7b', value: '7b'}
-      ];
-    default:
-      return [];
-  }
-}
-
-let GetGradeSelectionObject = (system) => {
-  let object = new Object;
-  const gradeSelection = GetGradeSelection(system);
-  gradeSelection.forEach( grade => {
-    object[grade.key] = grade.value;
-  })
-
-  return object;
-}
-
-let GetAdditionalGradeSelectionObject = (system) => {
-  let object = new Object;
-  const gradeSelection = GetAdditionalGradeSelection(system);
-  gradeSelection.forEach( grade => {
-    object[grade.key] = grade.value;
-  })
-
-  return object;
-}
-
-let GetDisplayRouteType = (type) => {
-  switch(type) {
-    case 'sport': return 'Sport';
-    case 'trad' : return 'Trad';
-    default: return '';
-  }
-}
-
-let GetDisplayGrade = (grade) => {
-  if( grade == undefined ) return '';
-  const gradeSelection = GetGradeSelectionObject(grade.system);
-  const additionalGradeSelection = GetAdditionalGradeSelectionObject(grade.system);
-  return (additionalGradeSelection[grade.additional_value] == undefined ) ? 
-  gradeSelection[grade.value] : `${gradeSelection[grade.value]} ${additionalGradeSelection[grade.additional_value]}`;
-}
+// let CreateObjectFromRow = (row) => {
+//   return {id: row.cells[1].innerText, name: row.cells[2].innerText, type: GetRouteTypeForStorage(row.cells[3].innerText)};
+// }
 
 let UpdateRouteTable = (topoData) => {
   var routeTable=document.getElementById("route-table-body");
   routeTable.innerHTML = '';
-  let html = '';
   if( topoData.routes != undefined ) {
-    topoData.routes.forEach(element => {
-      html += 
-      `<tr>
-        <td><input class="route-checkbox" type="checkbox" /></td>
-        <td>${element.id}</td>
-        <td>${element.name}</td>
-        <td>${GetDisplayRouteType(element.type)}</td>
-        <td>${GetDisplayGrade(element.grade)}</td>
-      </tr>`;
+    topoData.routes.forEach(route => {
+      var row = CreateRowFromObject(route);
+      routeTable.appendChild(row).onclick = event => {
+        if( event != undefined ) {
+          const checkBox = event.currentTarget.cells[0].children[0];
+          checkBox.checked = !checkBox.checked;
+        }
+      };
     });
   }
-
-  routeTable.innerHTML = html;
 }
 
-let GetRouteGradeValue = (system) => {
-  switch( system ) {
-    case 'uk':
-      return document.getElementById('route-grade').value;
-    case 'french':
-      return document.getElementById('route-grade').value;
+let GetRouteById = (topoData, id) => {
+  for( let route of topoData.routes ) {
+    if( route.id == id ) return route;
   }
+  return null;
 }
 
-let GetAdditionalRouteGradeValue = (system) => {
-  switch( system ) {
-    case 'uk':
-      return document.getElementById('additional-route-grade').value;
-    default:
-      return undefined;
+let GetSelectedRoute = (topoData) => {
+  const table = document.getElementById('route-table');
+  for (let row of table.rows) {
+    const checkBox = row.cells[0].children[0];
+    if( checkBox != undefined ) {
+      if( checkBox.checked ) {
+        return GetRouteById(topoData, row.cells[1].innerText);
+      }
+    }
   }
-}
-
-let GetGradeSelectionHtml = (system) => {
-  let gradeHtml = [];
-  gradeHtml.push(`<label for="route-grade">Route grade:</label>`);
-  gradeHtml.push(`<select id="route-grade"><option value="" selected="selected"></option>`);
-  const gradeDefs = GetGradeSelection(system);
-  gradeDefs.forEach( gradeDef => {
-    gradeHtml.push(`<option value="${gradeDef.key}">${gradeDef.value}</option>`);
-  });
-  gradeHtml.push('</select>');
-  const additionalGradeDefs = GetAdditionalGradeSelection(system);
-  if( additionalGradeDefs != null ) {
-    gradeHtml.push(`<select id="additional-route-grade"><option value="" selected="selected"></option>`);
-    additionalGradeDefs.forEach( def => {
-      gradeHtml.push(`<option value="${def.key}">${def.value}</option>`);
-    });
-    gradeHtml.push('</select>');
-  }
-  return gradeHtml.join('');
+  return null;
 }
 
 let LoadPage = async (topoId) => {
@@ -201,39 +106,33 @@ let LoadPage = async (topoId) => {
   AddMouseSupportToTable(base_image, canvas, routeTable);
 */
   const addButton = document.getElementById('add-route');
-  const dialog = document.getElementById('add-route-dialog');
   addButton.onclick = () => {
-    dialog.showModal();
-  }
-
-  dialog.addEventListener('close', async () => {
-    const rv = dialog.returnValue;
-    if( rv == 'confirm' ) {
-      const routeName = document.getElementById('route-name').value;
-      const routeType = document.getElementById('route-type').value;
-      const routeGradeSystem = document.getElementById('route-grade-system').value;
-      const routeGradeValue = GetRouteGradeValue(routeGradeSystem);
-      const addtionalRouteGradeValue = GetAdditionalRouteGradeValue(routeGradeSystem);
+    const route = {};
+    EditRouteDialog(route, (newRoute) => {
       topoData.routes = (topoData.routes == undefined) ? [] : topoData.routes;
-      let routeGrade = {system: routeGradeSystem, value: routeGradeValue, additional_value: addtionalRouteGradeValue};
-      let route = {id: topoData.routes.length + 1, name: routeName, type: routeType, grade: routeGrade };
-      topoData.routes.push(route);
+      newRoute.id = topoData.routes.length + 1;
+      topoData.routes.push(newRoute);
       UpdateRouteTable(topoData);
-      // TODO: check response
-      // {"acknowledged":true,"insertedId":"61726db5a7488601199a1b12"}
+    });
+  }
+  
+  const editRouteButton = document.getElementById('edit-route');
+  editRouteButton.onclick = () => {
+    const route = GetSelectedRoute(topoData);
+    if( route != null ) {
+      EditRouteDialog(route, (newRoute) => {
+        topoData.routes = (topoData.routes == undefined) ? [] : topoData.routes;
+        newRoute.id = topoData.routes.length + 1;
+        topoData.routes.push(newRoute);
+        UpdateRouteTable(topoData);
+      });
     }
-  });
+  }
   
   const saveButton = document.getElementById('save-changes');
   saveButton.onclick = () => {
     delete topoData.parent_data;
     UpdateTopoData(topoData);
-  }
-
-  const gradeSystem = document.getElementById('route-grade-system');
-  gradeSystem.onchange = () => {
-    const gradeSelection = document.getElementById('system-specific-grade');
-    gradeSelection.innerHTML = GetGradeSelectionHtml(gradeSystem.value);
   }
 }
 
