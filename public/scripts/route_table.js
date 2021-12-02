e204573399cf40838216ae5bc7701779477a15fc
1poundSOCKS <mathew.coburn@gmail.com>
2021-12-02 22:25:02 +0000

    Route table refactoring

--- public/scripts/route_table.js
+++ public/scripts/route_table.js
@@ -2,6 +2,60 @@
 // route table control
 //
 
+let UpdateRouteTable = (routes) => {
+  if( !routes ) return;
+  var routeTable=document.getElementById("route-table-body");
+  routeTable.innerHTML = '';
+  routes.forEach( (route, index) => {
+    var row = CreateRowFromObject(route, index+1);
+    routeTable.appendChild(row).onclick = event => {
+      if( event != undefined ) {
+        const checkBox = GetRowCheckBox(event.currentTarget);//.cells[0].children[0];
+        checkBox.checked = !checkBox.checked;
+      }
+    }
+  });
+}
+
+let MoveSelectedRouteUp = () => {
+  ShiftSelectedRoutes(-1);
+}
+
+let MoveSelectedRouteDown = () => {
+  ShiftSelectedRoutes(1);
+}
+
+let ShiftSelectedRoutes = (distance) => {
+  const rowsToMove = GetSelectedRows();
+  const rowIds = rowsToMove.map(row => GetRowId(row));
+  const rows = GetRowsAsArray();
+  rowsToMove.forEach( rowToMove => {
+    SwapRows(rows, rowToMove.rowIndex - 1, rowToMove.rowIndex - 1 + distance);
+  });
+  SelectRowsById(rowIds);
+}
+
+let SwapRows = (rows, firstIndex, secondIndex) => {
+  if( !rows[firstIndex] || !rows[secondIndex]) return;
+  const firstRowContent = rows[firstIndex].innerHTML;
+  const secondRowContent = rows[secondIndex].innerHTML;
+  rows[firstIndex].innerHTML = secondRowContent;
+  rows[secondIndex].innerHTML = firstRowContent;
+}
+
+let DeleteSelectedRows = () => {
+  const table=GetRouteTable();
+  for( let rowIndex = table.rows.length-1; rowIndex > 0; rowIndex-- ) {
+    console.log(`rowIndex=${rowIndex}`);
+    const row = table.rows[rowIndex];
+    if( RowIsSelected(row) ) table.deleteRow(rowIndex);
+  }
+}
+
+let RouteArrayToIdSet = routes => new Set(array.map(route => route.id));
+
+let GetRouteTable = () => routeTable=document.getElementById("route-table");
+
 let CreateRowFromObject = (route, index) => {
   let row = document.createElement('tr');
   row.innerHTML = 
@@ -16,23 +70,35 @@ let CreateRowFromObject = (route, index) => {
 
 let GetRowCheckBox = row => row.cells[1].children[0];
 
-let IsRowSelected = row => (GetRowCheckBox(row) != undefined) ? GetRowCheckBox(row).checked : false;
+let RowIsSelected = row => (GetRowCheckBox(row) != undefined) ? GetRowCheckBox(row).checked : false;
 
 let GetRowsAsArray = () => Array.from(document.getElementById('route-table').rows).filter(row => row.rowIndex > 0);
 
-let GetSelectedRows = () => GetRowsAsArray().filter(row => IsRowSelected(row));
+let GetSelectedRows = () => {
+  let selectedRows = [];
+  const rows = GetRowsAsArray();
+  rows.forEach( row => {
+    if( RowIsSelected(row) ) selectedRows.push(row);
+  });
+  return selectedRows;
+}
+
+let GetSelectedRowIds = () => {
+  return GetSelectedRows().map(row => GetRowId(row));
+}
+
+let SelectRowsById = (ids) => {
+  const idSet = new Set(ids);
+  GetRowsAsArray().forEach(row => {
+    if( idSet.has(GetRowId(row)) ) GetRowCheckBox(row).checked = true;
+  });
+}
 
 let GetUnselectedRows = () => GetRowsAsArray().filter(row => !IsRowSelected(row));
 
 let GetRowId = row => row.cells[0].innerText;
 
-let GetRouteFromRow = (topoData, row) => GetRouteById(topoData, GetRowId(row));
-
-let GetSelectedRoutes = topoData => GetSelectedRows().map(row => GetRouteFromRow(topoData, row));
-
-let GetUnselectedRoutes = topoData => GetUnselectedRows().map(row => GetRouteFromRow(topoData, row));
-
-let SetSelectedRoutes = (routes) => {
+let SelectRoutes = (routes) => {
   const rows = GetRowsAsArray();
   const selectedRows = rows.filter(row => {
     const rowId = GetRowId(row);
@@ -41,54 +107,4 @@ let SetSelectedRoutes = (routes) => {
   for( let row of selectedRows ) {
     GetRowCheckBox(row).checked = true;
   }
-}
-
-let UpdateRouteTable = (topoData) => {
-  var routeTable=document.getElementById("route-table-body");
-  routeTable.innerHTML = '';
-  if( topoData.routes != undefined ) {
-    topoData.routes.forEach((route, index) => {
-      var row = CreateRowFromObject(route, index+1);
-      routeTable.appendChild(row).onclick = event => {
-        if( event != undefined ) {
-          const checkBox = GetRowCheckBox(event.currentTarget);//.cells[0].children[0];
-          checkBox.checked = !checkBox.checked;
-        }
-      };
-    });
-  }
-}
-
-let EditSelectedRoute = topoData => {
-  const routes = GetSelectedRoutes(topoData);
-  if( routes.length != 1 ) return;
-  EditRouteDialog(routes[0], (newRoute) => {
-    UpdateRouteById(topoData, newRoute);
-    UpdateRouteTable(topoData);
-    SetSelectedRoutes(routes);
-  });
-}
-
-let MoveSelectedRouteUp = topoData => {
-  const routes = GetSelectedRoutes(topoData);
-  if( routes.length != 1 ) return;
-  const previousRoute = GetPreviousRoute(topoData, routes[0]);
-  SwapRoutes(topoData, routes[0], previousRoute);
-  UpdateRouteTable(topoData);
-  SetSelectedRoutes(routes);
-}
-
-let MoveSelectedRouteDown = topoData => {
-  const routes = GetSelectedRoutes(topoData);
-  if( routes.length != 1 ) return;
-  const nextRoute = GetNextRoute(topoData, routes[0]);
-  SwapRoutes(topoData, routes[0], nextRoute);
-  UpdateRouteTable(topoData);
-  SetSelectedRoutes(routes);
-}
-
-let DeleteSelectedRoutes = topoData => {
-  const unselectedRoutes = GetUnselectedRoutes(topoData);
-  topoData.routes = unselectedRoutes;
-  UpdateRouteTable(topoData);
 }
